98b8d89129c597a34672805170d5eaaee54e3976
tg0825 <tg0825@backpack.kr>
2018-06-11 02:05:08 +0900

    chapter9 mysql 끝

--- part3/chapter9-MySQL데이터베이스/9-4.board/app.js
+++ part3/chapter9-MySQL데이터베이스/9-4.board/app.js
@@ -23,20 +23,53 @@ app.listen(8888, function () {
 });
 
 app.get('/', function (req, res) {
-
+    fs.readFile('list.ejs', 'utf8', function (error, data) {
+        client.query('SELECT * FROM products', function (error, results) {
+            res.send(ejs.render(data, {
+                data: results
+            }));
+        });
+    });
 });
 app.get('/delete/:id', function (req, res) {
-
+    client.query('DELETE FROM products WHERE id=?', [req.params.id], function () {
+        res.redirect('/');
+    });
 });
 app.get('/insert', function (req, res) {
-
+    fs.readFile('insert.ejs', 'utf8', function (error, data) {
+        res.send(data);
+    });
 });
 app.post('/insert', function (req, res) {
-
+    var body = req.body;
+    client.query('INSERT INTO products (name, modelnumber, series) VALUES (?, ?, ?)', [
+        body.name,
+        body.modelnumber,
+        body.series
+    ], function () {
+        res.redirect('/');
+    });
 });
 app.get('/edit/:id', function (req, res) {
-
+    fs.readFile('edit.ejs', 'utf8', function (error, data) {
+        client.query('SELECT * FROM products WHERE id = ?', [
+            req.params.id
+        ], function (error, result) {
+            res.send(ejs.render(data, {
+                data: result[0]
+            }));
+        });
+    });
 });
 app.post('/edit/:id', function (req, res) {
-
+    var body = req.body;
+    client.query('UPDATE products SET name=?, modelnumber=?, series=? WHERE id=?', [
+        body.name,
+        body.modelnumber,
+        body.series,
+        req.params.id
+    ], function () {
+        res.redirect('/');
+    });
 });
