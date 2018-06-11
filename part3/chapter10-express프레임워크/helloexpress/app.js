a49a0c01eb34ede0e53613645d350e4a65a0b401
tg0825 <tg0825@backpack.kr>
2018-06-12 01:04:56 +0900

    10-4 완료

--- part3/chapter10-express프레임워크/helloexpress/app.js
+++ part3/chapter10-express프레임워크/helloexpress/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,6 +11,8 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
+// url 대소문자 구분
+app.set('case sensitive routes', true);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -17,6 +20,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+    secret: 'secret key',
+    resave: false,
+    saveUninitialized: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- part3/chapter10-express프레임워크/helloexpress/routes/index.js
+++ part3/chapter10-express프레임워크/helloexpress/routes/index.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/product', function (req, res) {
+    res.render('product', {
+        title: 'Product Page'
+    });
+});
+
+router.get('/product/insert', function (req, res) {
+    res.render('product/insert', {
+        title: 'insert Page'
+    });
+});
+
+router.get('/product/edit', function (req, res) {
+    res.render('edit', {
+        title: 'edit Page'
+    });
+});
+
 module.exports = router;
