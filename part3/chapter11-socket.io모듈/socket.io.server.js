507ca6a7510eb7feac4b190ff3d77ffc8c490853
tg0825 <tg0825@gmail.com>
2018-06-12 02:03:15 +0900

    통신 방식

--- part3/chapter11-socket.io모듈/socket.io.server.js
+++ part3/chapter11-socket.io모듈/socket.io.server.js
@@ -14,4 +14,25 @@ var server = http.createServer(function (req, res) {
     console.log('server run');
 });
 
+var id = 0;
 var io = socketio.listen(server);
+io.sockets.on('connection', function (socket) {
+    // rint 이벤트
+    socket.on('rint', function (data) {
+        // 클라이언트가 전송한 데이터 출력
+        console.log('client send Data:', data);
+
+        // socket.emit('smart', data);
+
+        // public 방식: 나 포함 모두에게
+        // io.sockets.emit('smart', data);
+
+        // broadcast 방식: 나를 제오한 모두에게
+        // socket.broadcast.emit('smart', data);
+
+        // private 방식
+        // id 설정
+        id = socket.id;
+        io.sockets.to(id).emit('smart', data);
+    });
+});
