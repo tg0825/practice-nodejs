c23a8a02325a6fb575cd1539ac191720913c529d
tg0825 <tg0825@gmail.com>
2018-06-12 01:35:41 +0900

    서버 생성

---
+++ part3/chapter11-socket.io모듈/socket.io.server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const fs = require('fs');
+const socketio = require('socket.io');
+
+var server = http.createServer(function (req, res) {
+    // load to page.html
+    fs.readFile('page.html', function (error, data) {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.end(data);
+    });
+}).listen(8888, function () {
+    console.log('server run');
+});
